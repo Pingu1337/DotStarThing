c82d3f015af96c98f6c3ba66a33358714f1fc1ec
ankan <ankan@DESKTOP-5E926FJ>
2021-08-28 12:12:52 +0200

    Add project files.

---
+++ DotStarThing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DotStarThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("Type a Whole Number:");
+            int num = int.Parse(Console.ReadLine());
+            Console.Clear();
+            int j = 0;
+
+                while (j < num)
+                {
+                    j++;
+                    for (int i = 0; i < num; i++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
