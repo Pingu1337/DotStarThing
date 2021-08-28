6c7590c96e851f5816e420609c9e1cb4815e0f59
Pingu1337 <herman.brunberg@utb.ecutbildning.se>
2021-08-28 13:08:32 +0200

    Added some colors.

--- DotStarThing/Program.cs
+++ DotStarThing/Program.cs
@@ -5,22 +5,32 @@ namespace DotStarThing
     class Program
     {
         static void Main(string[] args)
-        { 
-            Console.Write("Type a Whole Number:");
+        {
+            Console.Write("Type a whole number: ");
             int num = int.Parse(Console.ReadLine());
             Console.Clear();
             int j = 0;
 
-                while (j < num)
+            while (j < num)
+            {
+                j++;
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                for (int i = 0; i < num; i++)
                 {
-                    j++;
-                    for (int i = 0; i < num; i++)
-                    {
-                        Console.Write("*");
-                    }
-                    Console.WriteLine();
-                }
+                    Console.Write("*");
 
+                    if (i % 2 == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    }
+                    else if (i % 2 != 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
         }
